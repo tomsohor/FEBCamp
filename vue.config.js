87ffec1294f2dd270dc4c50785155da2db5e62ff
tomsohor <tomsohor55@gmail.com>
2020-10-04 21:17:57 +0700

    frontend

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 module.exports = {
   transpileDependencies: ["vuetify"],
-  outputDir: path.resolve(__dirname, "./server/public"),
+  outputDir: path.resolve(__dirname, "../server/public"),
   devServer: {
     proxy: {
       "/api": {
